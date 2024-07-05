b1f42308e438d1d41a9dc8cb66b53c48886bdf1e
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-07-04 21:14:29 -0600

    feat(): card value
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 02-blackjack/assets/js/app.js
+++ 02-blackjack/assets/js/app.js
@@ -48,3 +48,9 @@ const pedirCarta = () => {
 
 pedirCarta();
 
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
+
+console.log(valorCarta(pedirCarta()));
