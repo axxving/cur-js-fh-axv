5fdbb1dd99a031161b15f7cc8f232959e4c0c95c
axxving <alex.ing.hz@gmail.com>
2024-08-31 21:37:04 -0600

    feat: refactor codebase
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

---
+++ 05-blackjack-vite/src/blackjack/useCases/createDeck.js
@@ -0,0 +1,18 @@
+import _ from "underscore";
+
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+  for (let i = 2; i <= 10; i++) {
+    for (let tipo of tiposDeCarta) {
+      deck.push(i + tipo);
+    }
+  }
+
+  for (let tipo of tiposDeCarta) {
+    for (let esp of tiposEspeciales) {
+      deck.push(esp + tipo);
+    }
+  }
+
+  deck = _.shuffle(deck);
+  return deck;
+};
