39380958c78110f620c038034b3f3f6411a5a69e
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-09-03 19:58:19 -0600

    feat: docs and refactor
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 05-blackjack-vite/src/blackjack/useCases/index.js
+++ 05-blackjack-vite/src/blackjack/useCases/index.js
@@ -1,3 +1,4 @@
 export { crearDeck } from "./createDeck.js";
 export { pedirCarta } from "./askLetter.js";
 export { valorCarta } from "./valueLetter.js";
+export { turnoComputadora } from "./computerTurn.js";
